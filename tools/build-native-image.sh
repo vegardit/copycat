7e3cee4ea4b1420a5605009b6e4e8894395a96c7
sebthom <sebthom@users.noreply.github.com>
2023-01-12 17:03:04 +0100

    automatically collect native-image metadata

--- tools/build-native-image.sh
+++ tools/build-native-image.sh
@@ -17,6 +17,23 @@ cd "$(dirname ${BASH_SOURCE[0]})/../target/"
 input_jar=copycat-*-fat-minimized.jar
 output_binary=copycat-snapshot-linux-amd64
 
+# https://www.graalvm.org/22.3/reference-manual/native-image/metadata/AutomaticMetadataCollection/#tracing-agent
+rm -rf native-image-metadata
+function traceCall() {
+  echo "Tracing copycat ${@}..."
+  java -agentlib:native-image-agent=config-merge-dir=native-image-metadata/META-INF/native-image/ -jar $input_jar ${@} >/dev/null
+}
+
+traceCall --help
+traceCall --help
+traceCall --version
+traceCall sync --help
+traceCall watch --help
+
+rm -rf workdir
+install -D /dev/null workdir/source/dir1/file1
+traceCall sync workdir/source workdir/target
+
 native-image \
   -H:NativeLinkerOption=-no-pie `# do not to generate Position Independent Executables (PIE)` \
   -H:ReflectionConfigurationFiles=picocli-reflections.json \
@@ -31,6 +48,7 @@ native-image \
   --verbose \
   --report-unsupported-elements-at-runtime `# avoid: Unsupported type sun.awt.X11.XBaseWindow is reachable` \
   -Dfile.encoding=UTF-8 \
+  --class-path native-image-metadata \
   --class-path $input_jar \
   com.vegardit.copycat.CopyCatMain \
   $output_binary
