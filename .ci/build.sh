85f32239878524407188103747db8ff623eb0a7f
sebthom <sebthom@users.noreply.github.com>
2024-09-07 18:41:22 +0200

    refact: update build config

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #
 # SPDX-FileCopyrightText: © Vegard IT GmbH (https://vegardit.com)
+# SPDX-FileContributor: Sebastian Thomschke (Vegard IT GmbH)
 # SPDX-License-Identifier: Apache-2.0
-#
-# @author Sebastian Thomschke, Vegard IT GmbH
+# SPDX-ArtifactOfProjectHomePage: https://github.com/vegardit/copycat
 
 #####################
 # Script init
@@ -37,7 +37,7 @@ echo "###################################################"
 echo "# Determining GIT branch......                    #"
 echo "###################################################"
 GIT_BRANCH=$(git branch --show-current)
-echo "  -> GIT Branch: $GIT_BRANCH"; echo
+echo "  -> GIT Branch: $GIT_BRANCH"
 
 
 if ! hash mvn 2>/dev/null; then
@@ -66,34 +66,23 @@ fi
 
 echo
 echo "###################################################"
-echo "# Configuring JDK Class Data Sharing...           #"
-echo "###################################################"
-java_version=$(java -version 2>&1)
-echo "$java_version"
-# https://docs.oracle.com/javase/8/docs/technotes/guides/vm/class-data-sharing.html
-jdk_version_checksum=$(echo "$java_version" | md5sum | cut -f1 -d" ")
-if [[ ! -f $HOME/.xshare/$jdk_version_checksum ]]; then
-   echo "  -> Generating shared class data archive..."
-   mkdir -p $HOME/.xshare
-   java -Xshare:dump -XX:+UnlockDiagnosticVMOptions -XX:SharedArchiveFile=$HOME/.xshare/$jdk_version_checksum
-else
-   echo "  -> Reusing shared class data archive..."
-fi
-export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS:-} -Xshare:on -XX:+UnlockDiagnosticVMOptions -XX:SharedArchiveFile=$HOME/.xshare/$jdk_version_checksum"
-
-
-echo
-echo "###################################################"
 echo "# Configuring MAVEN_OPTS...                       #"
 echo "###################################################"
 MAVEN_OPTS="${MAVEN_OPTS:-}"
-MAVEN_OPTS="$MAVEN_OPTS -XX:+TieredCompilation -XX:TieredStopAtLevel=1" # https://zeroturnaround.com/rebellabs/your-maven-build-is-slow-speed-it-up/
-MAVEN_OPTS="$MAVEN_OPTS -Djava.security.egd=file:/dev/./urandom" # https://stackoverflow.com/questions/58991966/what-java-security-egd-option-is-for/59097932#59097932
-MAVEN_OPTS="$MAVEN_OPTS -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss,SSS" # https://stackoverflow.com/questions/5120470/how-to-time-the-different-stages-of-maven-execution/49494561#49494561
-export MAVEN_OPTS="$MAVEN_OPTS -Xmx1024m -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dhttps.protocols=TLSv1.2"
+MAVEN_OPTS+=" -Djava.security.egd=file:/dev/./urandom" # https://stackoverflow.com/questions/58991966/what-java-security-egd-option-is-for/59097932#59097932
+MAVEN_OPTS+=" -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss,SSS" # https://stackoverflow.com/questions/5120470/how-to-time-the-different-stages-of-maven-execution/49494561#49494561
+MAVEN_OPTS+=" -Xmx1024m -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dhttps.protocols=TLSv1.3,TLSv1.2"
 echo "  -> MAVEN_OPTS: $MAVEN_OPTS"
+export MAVEN_OPTS
 
-MAVEN_CLI_OPTS="-e -U --batch-mode --show-version --no-transfer-progress -s .ci/maven-settings.xml -t .ci/maven-toolchains.xml"
+MAVEN_CLI_OPTS="-e -U --batch-mode --show-version -s .ci/maven-settings.xml -t .ci/maven-toolchains.xml"
+if [[ -n ${GITEA_ACTIONS:-} || (-n ${CI:-} && -z ${ACT:-}) ]]; then # if running on a remote CI but not on local nektos/act runner
+  MAVEN_CLI_OPTS+=" --no-transfer-progress"
+fi
+if [[ -n ${ACT:-} ]]; then
+  MAVEN_CLI_OPTS+=" -Dformatter.validate.lineending=KEEP"
+fi
+echo "  -> MAVEN_CLI_OPTS: $MAVEN_CLI_OPTS"
 
 
 echo
@@ -101,8 +90,11 @@ echo "###################################################"
 echo "# Determining current Maven project version...    #"
 echo "###################################################"
 # https://stackoverflow.com/questions/3545292/how-to-get-maven-project-version-to-the-bash-command-line
-projectVersion="$(mvn -s .ci/maven-settings.xml help:evaluate -Dexpression=project.version -q -DforceStdout)"
+projectVersion=$(python -c "import xml.etree.ElementTree as ET; \
+  print(ET.parse(open('pom.xml')).getroot().find(  \
+  '{http://maven.apache.org/POM/4.0.0}version').text)")
 echo "  -> Current Version: $projectVersion"
+
 
 #
 # decide whether to perform a release build or build+deploy a snapshot version
@@ -137,6 +129,7 @@ if [[ ${projectVersion:-foo} == ${POM_CURRENT_VERSION:-bar} && ${MAY_CREATE_RELE
       -DreleaseVersion=${POM_RELEASE_VERSION} \
       -DdevelopmentVersion=${nextDevelopmentVersion} \
       help:active-profiles clean release:clean release:prepare release:perform \
+      | grep -v -e "\[INFO\] Download.* from repository-restored-from-cache" `# suppress download messages from repo restored from cache ` \
       | grep -v -e "\[INFO\]  .* \[0.0[0-9][0-9]s\]" # the grep command suppresses all lines from maven-buildtime-extension that report plugins with execution time <=99ms
 else
    echo
@@ -145,5 +138,6 @@ else
    echo "###################################################"
    mvn $MAVEN_CLI_OPTS "$@" \
       help:active-profiles clean verify \
+      | grep -v -e "\[INFO\] Download.* from repository-restored-from-cache" `# suppress download messages from repo restored from cache ` \
       | grep -v -e "\[INFO\]  .* \[0.0[0-9][0-9]s\]" # the grep command suppresses all lines from maven-buildtime-extension that report plugins with execution time <=99ms
 fi
