69c4589166f3a89f70d65022e5aac446936f625b
sebthom <sebthom@users.noreply.github.com>
2026-09-25 14:28:48 +0200

    ci(build): adopt shared Maven workflow and remove obsolete helpers

--- .ci/release-trigger.sh
+++ .ci/release-trigger.sh
@@ -1,4 +1,7 @@
-# this file is evaluated by .ci/build.sh
+#!/bin/sh
+# shellcheck disable=SC2034  # Ignore unused variables
+
+# this file is evaluated by shared workflow https://github.com/sebthom/gha-shared/blob/v1/.github/workflows/maven-build.yml
 
 POM_CURRENT_VERSION="0.0.0-SNAPSHOT" # perform release if pom.xml matches this version
 POM_RELEASE_VERSION="0.0.0" # next release version
